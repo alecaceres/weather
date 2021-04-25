d34c296b46f2d16fa6fa0790f036f1cc5609077b
Alejandro Cáceres <alejandro.caceres@wardenautomation.com>
2021-04-25 13:25:03 -0400

    Add change handler

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+    // form state
+    const [search, setSearch] = useState({
+        city: "",
+        country: ""
+    })
+
+    // get city and country
+    const {city, country} = search;
+
+    // change handler
+    const handleChange = e => {
+        // update state
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
         <form>
             <div className="input-field col s12">
@@ -8,6 +26,8 @@ const Form = () => {
                     type="text"
                     name="city"
                     id="city"
+                    value={city}
+                    onChange={handleChange}
                 />
                 <label htmlFor="city">City: </label>
 
@@ -15,8 +35,17 @@ const Form = () => {
                     <select
                         name="country"
                         id="country"
+                        value={country}
+                        onChange={handleChange}
                     >
                         <option value="">-- Select a country --</option>
+                        <option value="US">Estados Unidos</option>
+                        <option value="MX">México</option>
+                        <option value="AR">Argentina</option>
+                        <option value="CO">Colombia</option>
+                        <option value="CR">Costa Rica</option>
+                        <option value="ES">España</option>
+                        <option value="PE">Perú</option>
                     </select>
                     <label htmlFor="country">Country: </label>
                 </div>
